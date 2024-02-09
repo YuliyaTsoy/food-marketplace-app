338cf27d2b1de0a70c4f982bf40c723e50e9c650
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-09 10:02:57 -0700

    added auth.js file

---
+++ server/utils/auth.js
@@ -0,0 +1,14 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+// set token secret and expiration date
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+    AuthenticationError: new GraphQLError('Could not authenticate user.', {
+        extensions: {
+            code: 'UNAUTHENTICATED',
+        },
+    }),
+}
