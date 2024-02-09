130da354bb990e4f5e6681b33d5a3ba7f8e3f29d
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-09 15:00:02 -0500

    adds method to user schema to compare password

--- server/models/User.js
+++ server/models/User.js
@@ -44,6 +44,11 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 const User = model("User", userSchema);
 
 module.exports = User;
