27d99b681dc9a851cfcea3924f13efe43b9a9596
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-09 14:12:02 -0500

    checks for token validity

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -15,5 +15,15 @@ module.exports = {
 authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
+
+// ["Bearer", "<tokenvalue>"]
+if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
+
+ // if no token passed, return the req
+ if (!token) {
+  return req;
+}
 }
 }
