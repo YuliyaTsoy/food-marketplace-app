ce51f4be6848f7136575210c6a9cf9048e38c9a0
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-02-09 15:04:38 -0500

    add storeOwner attribute

--- server/models/Store.js
+++ server/models/Store.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const storeSchema = new Schema({});
+const storeSchema = new Schema({
+  storeOwner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
 const Store = mongoose.model("Store", storeSchema);
 module.exports = Store;
