dac45beee2837cf5fde225ec00473479a41022ea
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-02-10 14:35:56 -0500

    add category seed

--- server/config/seed.js
+++ server/config/seed.js
@@ -1,10 +1,18 @@
 const db = require("./connection");
-const { User } = require("../models");
+const { User, Category } = require("../models");
 const dropDB = require("./dropDB");
 
 db.once("open", async () => {
   await dropDB("User", "users");
+  await dropDB("Category", "categories");
 
+  const categories = await Category.insertMany([
+    { name: "Produce" },
+    { name: "Prepared Foods" },
+    { name: "Canned Gooods" },
+    { name: "Meat" },
+    { name: "Dairy" },
+  ]);
   await User.create({
     username: "jimmythetester",
     email: "jimmy@test.com",
