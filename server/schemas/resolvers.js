b6751576f1d27600b91d1c3e50bc6b0a979db77c
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-10 13:27:53 -0500

    adds outline to resolvers and puts in filler resolvers temporarily

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,13 @@ const { User, Product, Store, Category } = require("../models");
 //import for JWT authentication
 const { signToken, AuthenticationError } = require("../utils/auth");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    exampleQuery: {},
+  },
+  Mutation: {
+    exampleMutation: {},
+  },
+};
 
 module.exports = resolvers;
