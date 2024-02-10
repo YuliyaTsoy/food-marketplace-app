f996b7b25fdae173270b672c9c73a12db2bace80
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-10 15:18:29 -0500

    change imports in appjsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
