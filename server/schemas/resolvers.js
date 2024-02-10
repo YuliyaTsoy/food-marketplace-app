ca7ee2f9b16453926a2270404cf19beebb2d3d5e
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-10 14:32:06 -0500

    adds addOrder mutation to resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,21 +5,33 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      
-        if (context.user) {
-          userData = await User.findOne({ _id: context.user._id }).select(
-            "-__v -password"
-          );
-          return userData;
-        }
-        throw  AuthenticationError;
-      },
+      if (context.user) {
+        userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw AuthenticationError;
+    },
   },
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
+    },
+    addOrder: async (parent, { orderData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw AuthenticationError;
     },
   },
 };
