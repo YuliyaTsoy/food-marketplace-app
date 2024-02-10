c6a5062702a30172b357147c912172b2943c6702
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-10 16:36:49 -0700

    changed color and added a collor palette for easier manipulation

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,25 +6,32 @@ import { useLocation } from 'react-router-dom'
 function NavBar() {
     const currentPage = useLocation().pathname
 
+    const colorPalette = {
+        background: 'bg-red-200',
+        text: 'text-black-300',
+        hoverBg: 'bg-red-300',
+        currentBg: 'bg-red-800'
+    }
+
     const navigation = [
         { name: 'Home', href: '/', current: true },
-        { name: 'Login/Signup', href: '/Login', current: false },
+        { name: 'About', href: '/About', current: false },
         { name: 'Your Store', href: '/Store', current: false },
-        { name: 'About', href: '/', current: false },
+        { name: 'Login/Signup', href: '/Login', current: false },
     ]
 
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ')
     }
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className={colorPalette.background}>
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button className={`relative inline-flex items-center justify-center rounded-md p-2 text-black hover:${colorPalette.hoverBg} hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white`}>
                                     <span className="absolute -inset-0.5" />
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
@@ -54,7 +61,7 @@ function NavBar() {
                                                     href={item.href}
                                                     className={
                                                         classNames(
-                                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            item.current ? `${colorPalette.currentBg} text-white` : `${colorPalette.text} hover:${colorPalette.hoverBg} hover:text-white`,
                                                             'rounded-md px-3 py-2 text-sm font-medium'
                                                         )}
                                                 >
@@ -78,7 +85,7 @@ function NavBar() {
                                         as="a"
                                         href={item.href}
                                         className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            item.current ? `${colorPalette.currentBg} text-white` : `${colorPalette.text} hover:${colorPalette.hoverBg} hover:text-white`,
                                             'block rounded-md px-3 py-2 text-base font-medium'
                                         )}
                                     >
