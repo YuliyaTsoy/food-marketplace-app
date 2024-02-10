1da2a7432b88fcb56c86120b768e76f308b47c8b
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-10 14:35:07 -0500

    fixes addOrder mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,7 +24,7 @@ const resolvers = {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { savedBooks: bookData } },
+          { $push: { orders: orderData } },
           { new: true }
         );
 
