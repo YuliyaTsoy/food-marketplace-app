6dd1b2f99a6d00cfa371b94a5723286648791851
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-10 14:49:31 -0700

    added a current page conditional

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -34,6 +34,7 @@ function NavBar() {
                                     )}
                                 </Disclosure.Button>
                             </div>
+                            {/* Company logo */}
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex flex-shrink-0 items-center">
                                     <img
@@ -45,19 +46,22 @@ function NavBar() {
                                 {/* {these are the desktop view for the nav} */}
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'rounded-md px-3 py-2 text-sm font-medium'
-                                                )}
-                                                aria-current={item.current ? 'page' : undefined}
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
+                                        {navigation.map((item) => {
+                                            item.href == currentPage ? item.current = true : item.current = false;
+                                            return (
+                                                < a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    className={
+                                                        classNames(
+                                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            'rounded-md px-3 py-2 text-sm font-medium'
+                                                        )}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            )
+                                        })}
                                     </div>
                                 </div>
                             </div>
@@ -66,25 +70,28 @@ function NavBar() {
                     {/* mobile view tabs */}
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block rounded-md px-3 py-2 text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}
-                                >
-                                    <p className='text-center'> {item.name} </p>
-                                </Disclosure.Button>
-                            ))}
+                            {navigation.map((item) => {
+                                (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block rounded-md px-3 py-2 text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        <p className='text-center'> {item.name} </p>
+                                    </Disclosure.Button>
+                                )
+                            })}
                         </div>
                     </Disclosure.Panel>
                 </>
-            )}
-        </Disclosure>
+            )
+            }
+        </Disclosure >
     )
 }
 
