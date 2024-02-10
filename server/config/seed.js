ae6128c94bef997a374c5b8abe8d03fd63f29198
tjamesmcdonald <tjames.mcdonald@gmail.com>
2024-02-10 13:57:11 -0500

    Add user seed

---
+++ server/config/seed.js
@@ -0,0 +1,21 @@
+const db = require("./connection");
+const { User } = require("../models");
+const dropDB = require("./dropDB");
+
+db.once("open", async () => {
+  await dropDB("User", "users");
+
+  await User.create({
+    username: "jimmythetester",
+    email: "jimmy@test.com",
+    password: "password123",
+  });
+
+  await User.create({
+    username: "leonlemartin",
+    email: "leon@lemartin.com",
+    password: "supersecretpassword",
+  });
+
+  process.exit();
+});
