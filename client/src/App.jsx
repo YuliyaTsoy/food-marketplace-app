61df6579f18256b0174693ad7e088b13c56fa7c5
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-10 14:13:09 -0500

    App.jsx is now ready for header and footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {/* TODO: Put content here */}
-    </>
+    <div className='content'>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
   );
 }
+
 export default App
