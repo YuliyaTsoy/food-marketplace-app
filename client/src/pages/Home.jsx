4fb4dd09e076f259a9f413183683e4b31c247eb6
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-11 11:01:18 -0500

    help me

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,8 @@
 import Filter from '../components/Filter'
 
-<<<<<<< HEAD
 // dummy import of tomato image
 import {Tomato} from '../assets/samplepics/index'
-=======
+
 function Product() {
     return (
         <div className="product-card bg-red-200">
@@ -11,8 +10,6 @@ function Product() {
         </div>
     )
 };
->>>>>>> parent of 6fa4bd5 (Added sample pics, some minor styling, and a semi-functional Product component that currently lives in Home.jsx but probably will be refactored)
-
 
 export default function Home() {
     return (
