6fa4bd532b16288f034821f50929a3bc00ff0187
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-11 02:02:51 -0500

    Added sample pics, some minor styling, and a semi-functional Product component that currently lives in Home.jsx but probably will be refactored

---
+++ client/src/assets/samplepics/index.jsx
@@ -0,0 +1,22 @@
+// import sample food pics
+// this file is meant to compliment the seed data but in no way
+// serves as an implementation of product pictures
+import Tomato from './tomato.jpg'
+import Samosa from './samosa.jpg'
+import Potatoes from './potatoes.jpg'
+import FishTacos from './fish_tacos.jpg'
+import Empanadas from './empanadas.jpg'
+import Eggplant from './eggplant.jpg'
+import Carrots from './carrots.jpg'
+import BrusselSprouts from './brussel_sprouts.jpg'
+
+module.exports = {
+  Tomato,
+  Samosa,
+  Potatoes,
+  FishTacos,
+  Empanadas,
+  Eggplant,
+  Carrots,
+  BrusselSprouts
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,54 @@
+import React, { useState, useEffect, useRef, useMemo } from 'react'
+
 import Filter from '../components/Filter'
 
-function Product() {
-    return (
-        <div className="product-card bg-red-200">
-            Hellooooo
-        </div>
-    )
-};
+// dummy import of tomato image
+import Tomato from '../assets/tomato.jpg'
 
+// Title case a string. Guaranteed to work regardless of user input
+function titleCaseName(string) {
+    const capWords = [];
+    for (const word of string.split(' ')) {
+        capWords.push(word.substring(0, 1).toUpperCase() + word.substring(1));
+    }
+    return capWords.join(' ');
+}
+
+class Product extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // Create ref to this Product
+        this.ref = React.createRef();
+    }
+    render() {
+        const {name, image, price, storeName} = this.props;
+        return (
+            <div className="product-card bg-red-200 rounded-lg" ref={this.ref}>
+                <div className="product-picture-box">
+                    <img
+                    src={image}
+                    alt={`Product image of ${name}`}
+                    title={`Product image of ${name}`}
+                    loading="lazy" className="fit-picture" />
+                </div>
+                <div className="product-description">
+                    {/* price tag is trimmed down to 2 decimal points */}
+                    <h2 className="price-tag">{`$${price.toFixed(2)}`}</h2>
+                    <h3 className="product-name">{titleCaseName(name)}</h3>
+                    <h4 className="store-name">{storeName}</h4>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="font-bold">this is the home page</h1>
+        <div className="home-page">
+            <Filter />
             <div className="grid gap-4 grid-cols-4">
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
-                <Product />
+                <Product name="tomato" image={Tomato} price={432.891} storeName={"Luc's store"} />
             </div>
         </div>
     );
