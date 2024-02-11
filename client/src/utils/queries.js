2f188c59895838e8d170fa1eb736596abf14eb00
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-11 13:27:08 -0700

    mutation added to GET_MYSTORE

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,7 +2,36 @@ import { gql } from '@apollo/client';
 
 //get the user's store logged in to render
 
-export const GET_MYSTORE = gql``
+export const GET_MYSTORE = gql`
+mutation AddUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        _id
+        email
+        store {
+          _id
+          storeName
+          products {
+            _id
+            category {
+              _id
+              name
+            }
+            dateListed
+            description
+            image
+            name
+            price
+          }
+        }
+        username
+        orders {
+          _id
+        }
+      }
+    }
+}`;
 
 
 
