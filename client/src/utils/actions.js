07727bb3a10d75a0ffa81adaae072ec8531c743b
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-11 14:32:41 -0500

    adds action.js to client side

---
+++ client/src/utils/actions.js
@@ -0,0 +1,8 @@
+// update page with new products
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+
+// update categories with new category
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+
+// update current category onClick
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
