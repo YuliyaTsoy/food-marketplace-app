dda67a9604b4bd24323921691c58364817d610e7
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-11 13:34:45 -0700

    added login_user and add_user mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,87 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql``
+export const LOGIN_USER = gql`
+mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        store {
+          _id
+          storeName
+          products {
+            _id
+            category {
+              _id
+              name
+            }
+            dateListed
+            description
+            image
+            name
+            price
+          }
+        }
+        orders {
+          _id
+          category {
+            _id
+            name
+          }
+          dateListed
+          description
+          image
+          name
+          price
+          store {
+            _id
+            storeName
+          }
+        }
+      }
+    }
+}`;
 
-export const ADD_USER = gql``
+export const ADD_USER = gql`
+mutation AddUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {   
+        token
+        user {
+          _id
+          username
+          store {
+            _id
+            storeName
+            products {
+              _id
+              category {
+                _id
+                name
+              }
+              dateListed
+              description
+              image
+              name
+              price
+            }
+          }
+          orders {
+            _id
+            category {
+              _id
+              name
+            }
+            dateListed
+            description
+            image
+            name
+            price
+            store {
+              _id
+              storeName
+            }
+          }
+        }
+      }
+  }`; 
