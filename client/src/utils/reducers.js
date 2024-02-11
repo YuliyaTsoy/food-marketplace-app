4e10caccecc3bc906b2fb1217e1162eac1eef373
nickcatalano5 <nickcatalano24@gmail.com>
2024-02-11 14:41:23 -0500

    adds switch case for UPDATE_PRODUCTS reducer

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,3 +3,10 @@ import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
 } from "./actions";
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case UPDATE_PRODUCTS:
+      return { ...state, products: [action.products] };
+  }
+};
