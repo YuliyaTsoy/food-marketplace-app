ebbfe49c7d91d88a078baeacc27775114c771123
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-11 12:35:21 -0700

    brought apollo client components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,11 @@
 import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
