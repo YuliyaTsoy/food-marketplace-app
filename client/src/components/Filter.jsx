9fc778e4f28d96c3d23489ef0838d7f3c57f5be7
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-10 20:37:53 -0500

    Made Filter component that will be imported to home page

---
+++ client/src/components/Filter.jsx
@@ -0,0 +1,8 @@
+// Dummy Filter section. Need this file for spacing product listings on home page
+function Filter() {
+	return (
+		<div>
+		</div>
+	)
+}
+export default Filter;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,26 @@
+import Filter from '../components/Filter'
+
+function Product() {
+    return (
+        <div className="product-card bg-red-200">
+            Hellooooo
+        </div>
+    )
+};
+
+
 export default function Home() {
     return (
         <div>
             <h1 className="font-bold">this is the home page</h1>
-
+            <div className="grid gap-4 grid-cols-4">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
         </div>
     );
 }
