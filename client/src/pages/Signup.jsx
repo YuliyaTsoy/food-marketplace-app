668da3021f402295f7cf98e3bc6166b191cb45f7
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-12 08:28:26 -0700

    disabled button if inputs are empty

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -92,7 +92,9 @@ function Signup() {
                         </div>
                     </div>
                     <div>
-                        <button type="submit"
+                        <button
+                            type="submit"
+                            disabled={!(userFormData.username && userFormData.email && userFormData.password && userFormData.storeName)}
                             className="flex w-full justify-center rounded-md bg-red-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                             Sign in</button>
                     </div>
