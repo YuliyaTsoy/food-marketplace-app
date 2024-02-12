073a38b2df50a93c7c4f3b33087354f57f449d17
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-12 13:22:35 -0500

    adds ProductCard component initial function

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef, useMemo } from "react";
+
+const ProductCard= (props) => {
+  const { image, title, price, description, store } = props;
+  return (
+    <>
+
+    </>
+  );
+};
+export default ProductCard;
