2d2a1110de8a306a84c8ed9b6750562d3561824e
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-12 15:04:15 -0500

    renamed product card

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,15 +1,26 @@
 import React, { useState, useEffect, useRef, useMemo } from "react";
-
-import ProductCard from "../components/ProductCard.jsx";
+import { Routes, Route, useParams } from 'react-router-dom';
+import ProductCard from "../components/SilngeProductCard.jsx";
 // import { Carrots } from "../assets/samplepics/index.jsx";
 
 
-export default function Product(props) {
-  const [product, setProduct] = useState({});
+// export default function Product(props) {
+//   const [product, setProduct] = useState({});
    
-  return (
-    <>
-    <link rel="stylesheet" href="" />
-     </>
-  );
+//   return (
+//     <>
+//     <link rel="stylesheet" href="" />
+//      </>
+//   );
+// }
+
+function ProductPage () {
+    let { productId } = useParams();
+    return 
+    <Routes>
+      
+            <Route path = "/Product/:productId" element = {<ProductCard/>} />
+       
+    </Routes>
 }
+export default ProductPage
