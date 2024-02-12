2c512debfc8c009f1e3684a27b3a4e7fd8b963e4
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-12 13:38:47 -0500

    adds div to return image container

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,7 +4,11 @@ const ProductCard= (props) => {
   const { image, title, price, description, store } = props;
   return (
     <>
-
+     <div className="product-card lg:flex md:flex">
+        <div className="product-image-container ml-20">
+          <img className="product-picture-box" src={image} alt={title} />
+	</div>
+    </div>
     </>
   );
 };
