8892b3e530f5ef557e2b3bc635eb06a447b623f0
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-11 21:06:03 -0500

    adds carrot image as an example

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,9 +1,24 @@
+import React, { useState, useEffect, useRef, useMemo } from "react";
+import { Carrots } from "../assets/samplepics/index.jsx";
+
+const colorPalette = {
+  background: "bg-red-200",
+  text: "text-black-300",
+  hoverBg: "bg-red-300",
+  currentBg: "bg-red-800",
+};
+
 export default function Product() {
-    return (
-      <div>
-        <h1>Product Page</h1>
-        <p>Product Info goes here</p>
+  const [price, setPrice] = useState();
+
+  return (
+    <>
+      <div className="product-page">
+        <div className="product-image-container mx-20">
+          <img className="object-cover" src={Carrots} />
+        </div>
+       
       </div>
-    );
-  }
-  
+    </>
+  );
+}
