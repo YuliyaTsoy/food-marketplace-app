39bf5ff065e837a8b0c7c317e51ff22f7b6ee4e7
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-12 10:46:38 -0500

    moved produce details to the right if on a large screen

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -13,18 +13,19 @@ export default function Product() {
 
   return (
     <>
-      <div className="product-page">
+      <div className="product-page lg:flex">
         <div className="product-image-container mx-20">
           <img className="object-cover" src={Carrots} />
         </div>
-        <div className="product-details ml-10">
+        <div className="product-details ml-10 lg:flex-end m-6">
           <h1 className="text-xl px-5 py-2">Carrots</h1>
           <p className="px-5 py-2">Price: ${price}</p>
-          <p className=" px-5 py-2">
+          <p className="px-5 py-2">
             Description: Lorem ipsum, dolor sit amet consectetur adipisicing
             elit.
           </p>
-          <button className="bg-red-300 cursor-pointer hover:text-white rounded-full ml-10 mb-3 px-5 py-2">
+          <p className="px-5 py-2 mb-3">Store name:</p>
+          <button className="bg-red-300 cursor-pointer hover:text-white rounded-full ml-10 mt-8 mb-3 px-5 py-2">
             Add to store
           </button>
         </div>
