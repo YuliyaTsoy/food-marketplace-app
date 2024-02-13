ce53162f2650b8db9054ab7ff20b36bcf7690269
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-12 19:57:24 -0500

    attemt to get details on a  single product-needs some debugging

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,7 +8,9 @@ function ProductPage() {
     let { productId } = useParams();
     //   console.log(productId);
     const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
-   
+    console.log(data.products);
+    const product = data.products.findOne((product) => product._id === productId);
+    console.log(product);
     return (
       <div>
         <ProductCard product={product} />
