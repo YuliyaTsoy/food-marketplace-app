1ac11f21818d1cf1d7770fd91822327c2f3b7e2a
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-13 10:36:54 -0500

    changed find to filter method

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -8,8 +8,10 @@ function ProductPage() {
     let { productId } = useParams();
     //   console.log(productId);
     const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
-    console.log(data.products);
-    const product = data.products.findOne((product) => product._id === productId);
+    console.log(data);
+
+    const product =
+    data?.products.filter((product) => product._id === productId)[0] || {};
     console.log(product);
     return (
       <div>
