bfe95d4411fa45f5c6f436c8c212773d785c728b
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-13 11:47:13 -0500

    building out add product form and each input's handlers

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,10 +1,52 @@
-import ImageUploadDragOver from "../components/ImageUploadDragOver"
+import { useState } from "react";
+import ImageUploadDragOver from "../components/ImageUploadDragOver";
 
 export default function AddProduct() {
+
+    const [productName, setProductName] = useState("")
+    const [productPrice, setProductPrice] = useState(0);
+
+    function handleNameChange(props) {
+        setProductName(props.target.value);
+    }
+
+    function handlePriceChange(props) {
+        // This is a string and must be parsed first
+        let priceText = props.target.value;
+
+        const priceValue = Number.parseFloat(priceText);
+        console.log('props.target -> ', props.target);
+        console.log('priceValue -> ', priceValue);
+
+        // A valid price must not be a NaN or a negative number
+        if (!isNaN(priceValue) && priceValue >= 0) {
+            return setProductPrice(priceValue);
+        }
+
+        // Handle invalid prices below
+
+        // if invalidstring or negative number
+        if (isNaN(priceValue)) {
+            alert("Invalid product price");
+        }
+
+        if (priceValue < 0) {
+            alert("A product can't have a negative price!")
+        }
+    }
 
     function handleFormSubmit(e) {
         e.preventDefault();
         console.log('e -> ', e);
+
+        // If user attempts to submit only whitespace
+        if (!productName.trim().length) {
+            return alert("Product name must be a valid string!");
+        }
+
+
+
+        // clear form
     }
 
     return (
@@ -13,11 +55,11 @@ export default function AddProduct() {
             <form onSubmit={handleFormSubmit}>
                 <div className="flex flex-col">
                     <label htmlFor="product-name">Enter a product name</label>
-                    <input type="text" id="product-name" className="border-dashed rounded-lg border-2 border-slate-950 w-1/5"></input>
+                    <input type="text" id="product-name" className="border-dashed rounded-lg border-2 border-slate-950 w-1/5" onChange={handleNameChange}></input>
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="product-price">Enter a product price</label>
-                    <input type="text" inputMode="numeric" pattern="[0-9\.,]*" id="product-price" className="border-dashed rounded-lg border-2 border-slate-950 w-1/5"></input>
+                    <input type="text" inputMode="numeric" pattern="[0-9\.,]*" id="product-price" className="border-dashed rounded-lg border-2 border-slate-950 w-1/5" onChange={handlePriceChange}></input>
                 </div>
                 <div className="flex flex-col">
                     <ImageUploadDragOver />
@@ -25,6 +67,9 @@ export default function AddProduct() {
                 <div className="flex flex-col">
                     <label htmlFor="product-description">Make a Product Description</label>
                     <input type="text" id="product-description" className="border-dashed rounded-lg border-2 border-slate-950 w-1/5 h-1/5"></input>
+                </div>
+                <div className="flex flex-col">
+                    <input type="submit" value="Add Product!"/>
                 </div>
             </form>
         </div>
