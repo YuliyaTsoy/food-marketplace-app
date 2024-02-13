824cf1c1fbe8fe2da1ba9bc2a8f82e6d2af0f243
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-12 18:17:21 -0700

    changed searchbar html from tailwind

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,19 +1,27 @@
 function SearchBar() { }
 return (
-    <div class='max-w-md mx-auto'>
-        <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-            <div class="grid place-items-center h-full w-12 text-gray-300">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </div>
-
+    <form className="max-w-sm px-4">
+        <div className="relative">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute top-0 bottom-0 w-6 h-6 my-auto text-gray-400 left-3"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+            </svg>
             <input
-                class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
                 type="text"
-                id="search"
-                placeholder="Search something.." />
+                placeholder="Search"
+                className="w-full py-3 pl-12 pr-4 text-gray-500 border rounded-md outline-none bg-gray-50 focus:bg-white focus:border-indigo-600"
+            />
         </div>
-    </div>
+    </form>
 )
 export default SearchBar;
