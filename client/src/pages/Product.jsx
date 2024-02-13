2b299d1b6ead56f54104775e4e82c874f2051a16
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-13 12:59:30 -0500

    fixed bugs in the imports on the product page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import ProductCard from "../components/SilngeProductCard";
+import ProductCard from "../components/SingleProductCard";
 import { useQuery } from "@apollo/client";
-import { GET_ALL_PRODUCTS } from "../utils/queries";
+import { GET_ONE_PRODUCT } from "../utils/queries";
 
 function ProductPage() {
-    let { productId } = useParams();
-    //   console.log(productId);
-    const { loading, error, data } = useQuery(GET_ONE_PRODUCT, {
-      variables: { id: productId },
-    });
-    console.log(data);
-  
-    const product = data?.product || {};
-    console.log(product);
-    return (
-      <div>
-        <ProductCard product={product} />
-      </div>
-    );
+  let { productId } = useParams();
+  //   console.log(productId);
+  const { loading, error, data } = useQuery(GET_ONE_PRODUCT, {
+    variables: { id: productId },
+  });
+  console.log(data);
+
+  const product = data?.product || {};
+  console.log(product);
+  return (
+    <div>
+      <ProductCard product={product} />
+    </div>
+  );
 }
 
 export default ProductPage;
