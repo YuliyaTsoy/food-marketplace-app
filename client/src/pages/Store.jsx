477ea91f0c5a9a6ca9d31fd5f444659f83d84f3c
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-13 08:45:19 -0700

    useQuery to get user's profile

--- client/src/pages/Store.jsx
+++ client/src/pages/Store.jsx
@@ -1,3 +1,11 @@
+import ProductCard from '../components/ProductCard'
+import { useQuery } from '@apollo/client'
+import { GET_MYSTORE } from '../utils/queries'
+
 export default function Store() {
-    return <div></div>
+    // get user info using UseQuery method
+    const { loading, data } = useQuery(GET_MYSTORE)
+    const userStore = data?.myStore
+    console.log(userStore)
+    return <div><h1>this is your store page</h1></div>
 };
