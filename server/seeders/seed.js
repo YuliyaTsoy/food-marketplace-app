27f9107fec5b1e929c974e4f1626c216f04555c6
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-14 14:04:35 -0500

    Image seeding works nowgit add -A

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -7,6 +7,20 @@ const dropDB = require("./dropDB");
 // into a base 64 that can be stored in our DB and then served to the user
 const imageToBase64 = require('image-to-base64');
 
+// Need path library because relative paths don't seem to work with image-to-base-64
+const path = require("path");
+
+// image getting function wrapper
+const getImage = async (relPath) => {
+  try {
+    return await imageToBase64(path.join(
+      __dirname, "samplepics", relPath)
+      );
+  } catch(err) {
+    console.error(err);
+    return "";
+  }
+}
 
 db.once("open", async () => {
   await dropDB("User", "users");
@@ -25,79 +39,92 @@ db.once("open", async () => {
   // inefficient way of getting a category id from a category name
   // I dont want to remember 
   const catId = (name) => categories.find((category) => category.name === name)?._id;
-
+  ///home/luct/bootcamp/project3/food-marketplace-app/server/seeders/samplepics/carrots.jpg
   const products = await Product.insertMany([
     {
       name: "Carrots",
       price: 2.99,
       category: catId("Vegetables"),
       description: "Just harvested from my garden",
+      image: await getImage("carrots.jpg"),
+      //lister: 
     },
     {
       name: "Potatoes",
       price: 3.99,
       category: catId("Vegetables"),
       description: "very earthy taste",
+      image: await getImage("potatoes.jpg")
     },
     {
       name: "Brussel Sprouts",
       price: 1.99,
       category: catId("Vegetables"),
       description: "Very tasty when roasted",
+      image: await getImage("brussel_sprouts.jpg")
     },
     {
       name: "Samosas",
       price: 6.99,
       category: catId("Prepared Goods"),
       description: "made with love",
+      image: await getImage("samosa.jpg")
     },
     {
       name: "Empanadas",
       price: 7.99,
       category: catId("Prepared Goods"),
       description: "made with love",
+      image: await getImage("empanadas.jpg")
     },
     {
       name: "Fish Tacos",
       price: 17.52,
       category: catId("Prepared Goods"),
       description: "World famous fish tacos",
+      image: await getImage("fish_tacos.jpg")
     },
     {
       name: "Canned Peas",
       price: 3.99,
       category: catId("Canned Goods"),
       description: "hate peas, please take",
+      image: await getImage("canned_peas.jpg")
     },
     {
       name: "Brisket",
       price: 13.99,
       category: catId("Meat"),
       description: "Very tender",
+      image: await getImage("brisket.jpg")
     },
     {
       name: "Homemade Mozzarella Cheese",
       price: 5.99,
       category: catId("Dairy"),
       description: "Made in my bathtub",
+      image: await getImage("mozzarella.jpg")
     },
     {
       name: "Gouda",
       price: 4.99,
       category: catId("Dairy"),
       description: "Some good gouda",
+      image: await getImage("gouda.jpg")
     },
     {
       name: "Eggplant",
       price: 9.99,
       category: catId("Fruit"),
-      description: "From Luc's rooftop garden"
+      description: "From Luc's rooftop garden",
+      image: await getImage("eggplant.jpg")
     },
     {
       name: "Tomato",
       price: 21.34,
       category: catId("Fruit"),
-      description: "Soooo sweet!"
+      description: "Soooo sweet!",
+      image: await getImage("tomato.jpg")
     }
   ]);
   await User.create({
