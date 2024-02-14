929cd24384380dae48d9b4d50ab053be8dd5a964
Yuliya Tsoy <yuliya@Yuliyas-MacBook-Pro.local>
2024-02-14 13:35:53 -0500

    added about paragraph

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,9 @@ function About() {
     <div className='text-4xl text-center font-bold'>
       <h1>Satisfy Your Every Craving</h1></div>
       <hr/>
-      
+      <div>
+        <p>Fresh Finder is an online marketplace for local food that connects consumers with local farmers, producers, and artisans in their area. This application provides a convenient way for people to access fresh, locally sourced products without having to visit physical farmers' markets or stores. Users can browse through a wide range of food items, including fruits, vegetables, meats, dairy products, baked goods, and more. Our marketplace prioritizes sustainability, supporting small-scale farmers, and promoting organic or ethically produced food. Fresh Finder offers opportunity to enjoy the benefits of fresh food while supporting local small businesses.</p>
+      </div>
     </>
   );
 }
