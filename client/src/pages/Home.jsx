686cec0171866b56a2086c70e006dd8bcd2b8acc
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-13 17:32:06 -0700

    brings data with mutation get all products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,11 +3,18 @@ import { useState, useEffect, useRef, useMemo } from 'react'
 import Filter from '../components/Filter'
 import ProductCard from '../components/ProductCard';
 import SearchBar from '../components/SearchBar';
+import { useQuery, useMutation } from '@apollo/client'
+import { GET_ALL_PRODUCTS } from '../utils/queries';
 
 // dummy imports
 import { Tomato, Samosa, Potatoes, FishTacos, Brisket, Eggplant, Empanadas, Gouda } from '../assets/samplepics/index'
 
+
 export default function Home() {
+    //get all products from mutation GET_ALL_PRODUCTS
+    const { loading, data } = useQuery(GET_ALL_PRODUCTS)
+    const allProducts = data?.products || []
+    console.log(allProducts)
     return (
         <>
             <div className='flex justify-center md:mx-32 sm:mx-20'>
