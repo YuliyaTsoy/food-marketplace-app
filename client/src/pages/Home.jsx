a0f7b54f5fc5fb939edf191b4d021f3cec4e9808
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-13 17:41:07 -0700

    will render a sample picture for now

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,22 +36,15 @@ export default function Home() {
                                 <ProductCard
                                     key={product._id}
                                     name={product.name}
-                                    image={product.image}
+                                    image={Samosa}
+                                    // image={product.image}
                                     price={product.price}
-                                    // storeName={product.lister.storeName}
+                                    storeName={product.lister?.storeName}
                                 />
 
                             </>
                         )
                     })}
-                    {/* <ProductCard name="tomato" image={Tomato} price={432.891} storeName={"Luc's store"} />
-                    <ProductCard name="samosa" image={Samosa} price={12.32} storeName={"A store with way way too long of a name"} />
-                    <ProductCard name="potato" image={Potatoes} price={6.99} storeName={"my potato store"} />
-                    <ProductCard name="fish taco" image={FishTacos} price={17.52} storeName={"Luc's famous tacos"} />
-                    <ProductCard name="brisket" image={Brisket} price={32.90} storeName={"arbees"} />
-                    <ProductCard name="eggplant" image={Eggplant} price={9} storeName={"Luc's roof top garden"} />
-                    <ProductCard name="empanadas" image={Empanadas} price={13.41} storeName={"my abuelita"} />
-                    <ProductCard name="gouda" image={Gouda} price={22.22} storeName="some good gouda" /> */}
                 </div>
             </div>
         </>
